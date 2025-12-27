d1df572e18ee93b41a02c7df078f4abff8e6831d
Mohammad Heydari <Hmohammad2520@gmail.com>
2025-12-27 15:38:26 +0330

    Session_1

---
+++ Session_1.py
@@ -0,0 +1,36 @@
+# float, int, string, bool, list, tuple, set, dict, None
+
+### int ###
+integer1 = 10
+integer2 = int(30)
+integer3 = int(True)
+integer4 = int('11')
+
+### float ###
+float1 = 10.4
+float2 = float(10)
+float3 = float('12.6')
+
+### bool ###
+bool1 = True
+bool2 = False
+bool3 = bool(-1)
+bool4 = bool('')
+
+
+### str ####
+string1 = ''
+string2 = "Salam \nMohammad goft:\\"
+string3 = """
+Salam
+Khodafez
+"""
+string4 = f'do do ta: {2*2}'
+
+print(string4)
+
+### bytes ###
+bytes1 = b'Salam'
+
+
+
